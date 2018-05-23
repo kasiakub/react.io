d77d149a9cf2fd58fb2d41dcad8a2af981daf210
kasiakub <36821441+kasiakub@users.noreply.github.com>
2018-05-23 21:40:02 +0200

    project files

---
+++ src/components/robots.js
@@ -0,0 +1,36 @@
+export const robots = [
+     {
+        id: 1,
+        name: "Bartek Kubica",
+        username: 'Mis',
+        email: "bartlomiej.kubica89@gmail.com"
+    },
+    
+    {
+        id: 2,
+        name: "Kasia Kubica",
+        username: 'Malunia',
+        email: "kasialesniak@poczta.fm"
+    },
+    
+    {
+        id: 3,
+        name: 'Mateusz Kubica',
+        username: 'Meter',
+        email: "meter@gmail.com"
+    },
+    
+    {
+        id: 4,
+        name: "Ola Skoczek",
+        username: "Skoti",
+        email: "skoti90@gmail.com"
+    },
+    
+     {
+        id: 5,
+        name: "Ola z Miklaszowa",
+        username: "Alex",
+        email: "alemiklasz@gmail.com"
+    }
+]
